3c90fa39a7f9111f5d9a3672a4cdd936e0955f2e
JiroCab <Jirocab.89@gmail.com>
2024-10-24 21:35:54 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -15,9 +15,10 @@ lightningBul.collidesAir = true;
 
 UnitTypes.mace.speed = 0.7; 
 UnitTypes.mace.targetAir = false;
+UnitTypes.mace.weapons.get(0).bullet.damage = 23;
+UnitTypes.mace.weapons.get(0).reload = 60 * 0.75;
 UnitTypes.mace.weapons.get(0).bullet.collidesAir = false;
 UnitTypes.mace.weapons.get(0).bullet.pierceBuilding = false;
-UnitTypes.mace.weapons.get(0).bullet.buildingDamageMultiplier = 0.75;
 
 let fortressAAWeapon = new Weapon("missiles-mount");
 fortressAAWeapon.bullet = new BasicBulletType(5, 150); // syntax speed: (speed, dmg)
